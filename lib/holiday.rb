ea1e29a963048c71396e6d774bff4bf3261c4b87
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 21:45:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,8 +69,8 @@ def all_holidays_with_bbq(holiday_hash)
   result == []
   # k1 = season
   holiday_hash.each do |k1,v1|
-    # k2 = holiday 
-    value.each do|k2, v2|
+    # k2 = holiday
+    v1.each do|k2, v2|
       result << k2 if v2.any? {|x| x == "BBQ"
     end
   end

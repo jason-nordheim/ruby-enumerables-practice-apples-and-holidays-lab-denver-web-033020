914ddf6b9c05b88bc5de629a6ee3f5467458a230
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 21:29:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key,value|
     puts "#{key.capitalize}:"
     value.each do |key, value|
-      puts "  #{key.to_s.gsub("_"," ").upcase}: #{value.join(", ")}"
+      puts "  #{key.to_s.gsub("_"," ").titlecase}: #{value.join(", ")}"
     end
   end
 end

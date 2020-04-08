e26ca4ef1f2073e225827150a22d3b1c5117b9e6
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 21:24:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |key,value| 
+    puts "#{key}:"
+    value.each do |key, value| 
+      output = "#{key}: " value.join(", ")
+      
+    end 
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)

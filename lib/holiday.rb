1a752aa215e9e131339f1e2b250c5a5cf76a7256
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-08 21:43:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  result == [] 
   holiday_hash.each {|k1,v1| value.each {|k2, v2| v2.any? {|x| x == "BBQ"}}}
 end
 
